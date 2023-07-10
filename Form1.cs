5a31df4b1fe901cb2bd276f2b9cb2dba2b636179
VladislaVFEABE <104216804+VladislaVFEABE@users.noreply.github.com>
2023-07-10 17:13:23 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MessagingToolkit.QRCode.Codec;
+using MessagingToolkit.QRCode.Codec.Data;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string QRText = textBox1.Text; //Считка текста из textbox1
+            QRCodeEncoder encoder = new QRCodeEncoder();
+            Bitmap qrcode = encoder.Encode(QRText,Encoding.UTF8);//кодировка слова полученного из textbox1 в переменную qrcode
+            pictureBox1.Image = qrcode as Image;//вывод qrcode переменной в виде изображения
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "PNG|*.png|JPEG|*.jpg|GIF|*.gif|BMP|*.bmp";
+            if(save.ShowDialog()== System.Windows.Forms.DialogResult.OK)
+            {
+                pictureBox1.Image.Save(save.FileName);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog load = new OpenFileDialog();
+            if(load.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                pictureBox1.ImageLocation = load.FileName;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            QRCodeDecoder decoder = new QRCodeDecoder();
+            MessageBox.Show(decoder.decode(new QRCodeBitmapImage(pictureBox1.Image as Bitmap),Encoding.UTF8));
+        }
+    }
+}
